272e860c4a0f4b1ebcf2dfe68d77a42128107c2e
kumaran <KumaranM>
2025-03-23 00:13:32 +0530

    added .env and included the import of it

--- src/main/java/com/customerorder/CustomerOrder/CustomerOrderApplication.java
+++ src/main/java/com/customerorder/CustomerOrder/CustomerOrderApplication.java
@@ -1,4 +1,5 @@
 package com.customerorder.CustomerOrder;
+import io.github.cdimascio.dotenv.Dotenv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CustomerOrderApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
 		SpringApplication.run(CustomerOrderApplication.class, args);
 	}
 
